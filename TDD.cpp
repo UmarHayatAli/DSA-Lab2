492467dac8e01b6c0526a1007fd6267581efba77
UmarHayatAli <ummarh760@gmail.com>
2026-09-18 18:05:56 +0500

    Test Function corrected. Now Duplicate value test pass also

--- TDD.cpp
+++ TDD.cpp
@@ -8,7 +8,7 @@ bool isSorted(const int *arr, const int size) {
 
     for (int i = 0; i < size - 1; i++) {
         // The intentional bug: this will fail on duplicate numbers
-        if (arr[i] >= arr[i + 1]) { 
+        if (arr[i] > arr[i + 1]) { 
             return false;
         }
     }
